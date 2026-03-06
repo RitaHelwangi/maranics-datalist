d86e068d8e0fb222737d3945a3c6bb49ba7d0435
Rita Helwangi <rita.helwangi@gmail.com>
2026-03-06 05:02:19 +0100

    Vite proxy config to resolve CORS for Maranics API

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/maranics-datalist/" : "/",
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+	base: process.env.NODE_ENV === "production" ? "/maranics-datalist/" : "/",
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "https://flow-manager.cloud.preprod.maranics-staging.com",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, "/api"),
+			},
+		},
+	},
 });
